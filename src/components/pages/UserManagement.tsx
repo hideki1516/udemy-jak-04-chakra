4da3246dffcd26de9910714352f1bc4f471c5a17
hideki <hideki.1516.web@gmail.com>
2022-03-28 13:59:10 +0900

    74-UserManagement

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,7 +1,17 @@
+import { Wrap, WrapItem } from "@chakra-ui/react";
 import { memo, VFC } from "react";
+import { UserCard } from "../organisms/layout/user/UserCard";
 
 export const UserManagement: VFC = memo(() => {
   return (
-    <p>ユーザー管理画面ページです</p>
+    <Wrap p={{ base: 4, md: 10 }}>
+      <WrapItem>
+        <UserCard
+          imageUrl="https://source.unsplash.com/random"
+          userName="じゃけぇ"
+          fullName="Takumi Okada"
+        />
+      </WrapItem>
+    </Wrap>
   );
 });
