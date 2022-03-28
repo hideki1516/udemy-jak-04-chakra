9319951bef1a2e18443beecee5182c606beaaac5
hideki <hideki.1516.web@gmail.com>
2022-03-28 21:47:53 +0900

    75-UserManagement-state

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,17 +1,41 @@
-import { Wrap, WrapItem } from "@chakra-ui/react";
-import { memo, VFC } from "react";
+import { memo, useEffect, VFC } from "react";
+import {
+  Box,
+  Center,
+  SimpleGrid,
+  Spinner,
+  Wrap,
+  WrapItem,
+} from "@chakra-ui/react";
+
 import { UserCard } from "../organisms/layout/user/UserCard";
+import { useAllUsers } from "../../hooks/useAllUsers";
 
 export const UserManagement: VFC = memo(() => {
+  const { getUsers, loading, users } = useAllUsers();
+
+  // 画面表示時（初回時）にgetUsers()を処理
+  useEffect(() => getUsers(), []);
+
   return (
-    <Wrap p={{ base: 4, md: 10 }}>
-      <WrapItem>
-        <UserCard
-          imageUrl="https://source.unsplash.com/random"
-          userName="じゃけぇ"
-          fullName="Takumi Okada"
-        />
-      </WrapItem>
-    </Wrap>
+    <>
+      {loading ? (
+        <Center h="100vh">
+          <Spinner />
+        </Center>
+      ) : (
+        <Wrap p={{ base: 4, md: 10 }} justify="center">
+          {users.map((user) => (
+            <WrapItem key={user.id}>
+              <UserCard
+                imageUrl="https://source.unsplash.com/random"
+                userName={user.username}
+                fullName={user.name}
+              />
+            </WrapItem>
+          ))}
+        </Wrap>
+      )}
+    </>
   );
 });
